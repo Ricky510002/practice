0a5acfafee7ea34cddee429bebcb0643c46c2df3
Ryuki Goto <rth510ryu@gmail.com>
2021-07-01 21:49:49 +0900

    C - Low Elements

--- 1.js
+++ 1.js
@@ -1,19 +1,18 @@
-//A - Range Product
+//C - Low Elements  / 
 "use strict";
 
 const main = (lines) => {
-  const [a, b] = lines[0].split(" ").map(Number);
-  if (0 < a && a <= b) {
-    console.log("Positive");
-  } else if (a <= 0 && 0 <= b) {
-    console.log("Zero");
-  } else if (a <= b && b < 0) {
-    if ((b - a + 1) % 2 === 0) {
-      console.log("Positive");
-    } else {
-      console.log("Negative");
+  const n = Number(lines[0]);
+  const p = lines[1].split(" ").map(Number);
+  let min = p[0];
+  let cnt = 1;
+  for (let i = 1; i <= n; i++) {
+    min = Math.min(min, p[i]);
+    if (min == p[i]) {
+      cnt++;
     }
   }
+  console.log(cnt);
 };
 
 main(require("fs").readFileSync("stdin.txt", "utf8").trim().split("\n"));
