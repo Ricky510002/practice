2aa537302098491bf96a8e422b893e386362d903
Ryuki Goto <rth510ryu@gmail.com>
2021-07-03 10:00:18 +0900

    B - Palindromic Numbers

--- 1.js
+++ 1.js
@@ -1,20 +1,17 @@
-//C - Coloring Colorfully
+//B - Palindromic Numbers
 "use strict";
 
 const main = (lines) => {
-  const s = lines[0].split("");
-  let cnt1 = 0;
-  for (let i = 0; i < s.length; i++) {
-    const c = i % 2 ? "0" : "1";
-    if (s[i] !== c) cnt1++;
-  }
-  let cnt2 = 0;
-  for (let i = 0; i < s.length; i++) {
-    const c = i % 2 ? "1" : "0";
-    if (s[i] !== c) cnt2++;
+  const [a, b] = lines[0].split(" ").map(Number);
+  let ans = 0;
+  for (let i = a; i <= b; i++) {
+    const n = i.toString().split("").reverse().join("");
+    if (n === i.toString()) {
+      ans++;
+    }
   }
 
-  console.log(Math.min(cnt1, cnt2));
+  console.log(ans);
 };
 
 main(require("fs").readFileSync("stdin.txt", "utf8").trim().split("\n"));
