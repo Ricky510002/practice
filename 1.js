05b75b2781cfc78c37b07e5cc86c5f17615ba3c0
Ryuki Goto <rth510ryu@gmail.com>
2021-07-07 13:55:29 +0900

    C - Tax Increase

--- 1.js
+++ 1.js
@@ -1,14 +1,15 @@
-//B - Small and Large Integers
+//C - Tax Increase
 "use strict";
 
 const main = (lines) => {
-  const [a, b, k] = lines[0].split(" ").map(Number);
-
-  for (let i = a; i <= b; i++) {
-    if (i < a + k || i > b - k) {
+  const [a, b] = lines[0].split(" ").map(Number);
+  for (let i = 1; i <= 1000; i++) {
+    if (Math.floor(i * 0.08) === a && Math.floor(i * 0.1) === b) {
       console.log(i);
+      return;
     }
   }
+  console.log(-1);
 };
 
 main(require("fs").readFileSync("stdin.txt", "utf8").trim().split("\n"));
