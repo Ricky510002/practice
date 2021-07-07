d759cef29006dfc566665cd047a1424290682592
Ryuki Goto <rth510ryu@gmail.com>
2021-07-07 15:13:19 +0900

    B - String Rotation

--- 1.js
+++ 1.js
@@ -1,19 +1,24 @@
-//B - Contest with Drinks Easy
+//B - String Rotation
 "use strict";
 
 const main = (lines) => {
-  const N = Number(lines[0]);
-  const M = Number(lines[2]);
+  let S = lines[0];
+  let T = lines[1];
+  for (let i = 0; i < S.length; i++) {
+    S = S.split("");
 
-  for (let i = 0; i < M; i++) {
-    let T = lines[1].split(" ").map(Number);
-    let line = lines[i + 3].split(" ").map(Number);
-    let p = line[0];
-    let x = line[1];
-    T[p - 1] = x;
-    let ans = T.reduce((sum, element) => sum + element, 0);
-    console.log(ans);
+    // 末尾を先頭に複製
+    S.unshift(S[S.length - 1]);
+    //末尾削除
+    S.pop();
+    S = S.join("");
+
+    if (S == T) {
+      console.log("Yes");
+      return;
+    }
   }
+  console.log("No");
 };
 
 main(require("fs").readFileSync("stdin.txt", "utf8").trim().split("\n"));
