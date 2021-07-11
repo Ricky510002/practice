003a6c8258b88e5e2538b5e05948e67cb2b6e084
Ryuki Goto <rth510ryu@gmail.com>
2021-07-11 11:27:36 +0900

    B - Can you buy them all?

--- b.js
+++ b.js
@@ -1,6 +1,25 @@
+//B - Can you buy them all?
+"use strict";
+
 const main = (lines) => {
-  
-  
+  const [n, x] = lines[0].split(" ").map(Number);
+  let a = lines[1].split(" ").map(Number);
+
+  let total = 0;
+  for (let i = 0; i < n; i++) {
+    if (i % 2 === 0) {
+      total += a[i];
+    } else {
+      let a2 = a[i] - 1;
+      total += a2;
+    }
+  }
+
+  if (total <= x) {
+    console.log("Yes");
+  } else {
+    console.log("No");
+  }
 };
 
 main(require("fs").readFileSync("b.txt", "utf8").trim().split("\n"));
