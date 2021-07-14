5122fe0405ec9939002ee8b7bf9912bf159df6c1
Ryuki Goto <rth510ryu@gmail.com>
2021-07-14 15:39:55 +0900

    C - Christmas Eve

--- 1.js
+++ 1.js
@@ -1,12 +1,19 @@
-//C - Half and Half
+//C - Christmas Eve
 "use strict";
 
 const main = (lines) => {
-  let [A, B, C, X, Y] = lines[0].split(" ").map(Number);
-  let price1 = A * X + B * Y;
-  let price2 = 2 * C * Math.min(X, Y) + Math.abs(X - Y) * (X - Y > 0 ? A : B);
-  let price3 = 2 * C * Math.max(X, Y);
-  console.log(Math.min(price1, price2, price3));
+  const [N, K] = lines[0].split(" ").map(Number);
+  lines.shift();
+  const sorted = lines.sort((a, b) => a - b);
+
+  let min = 10000000000;
+  for (let i = 0; i <= N - K; i++) {
+    if (min > sorted[i + K - 1] - sorted[i]) {
+      min = sorted[i + K - 1] - sorted[i];
+    }
+  }
+
+  console.log(min);
 };
 
 main(require("fs").readFileSync("stdin.txt", "utf8").trim().split("\n"));
