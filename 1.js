80871965db53ed05101757ad5b7e72975449c6be
Ryuki Goto <rth510ryu@gmail.com>
2021-07-15 12:38:43 +0900

    C - Build Stairs

--- 1.js
+++ 1.js
@@ -1,19 +1,19 @@
-//A - AtCoder Group Contest
+//C - Build Stairs
 "use strict";
 
 const main = (lines) => {
-  let n = Number(lines[0]);
-  let a = lines[1].split(" ").map(Number);
-  let ans = 0;
-  a.sort((a, b) => a - b);
+  const N = Number(lines[0]);
+  const H = lines[1].split(" ").map(Number);
 
-  for (let i = 0; i < n * 3; i++) {
-    if (i >= n) {
-      if ((i - n) % 2 == 0) ans = ans + parseInt(a[i], 10);
+  let max = 0;
+  for (let i = 0; i < N; i++) {
+    if (max - H[i] > 1) {
+      console.log("No");
+      return;
     }
+    max = Math.max(max, H[i]);
   }
-
-  console.log(ans);
+  console.log("Yes");
 };
 
 main(require("fs").readFileSync("stdin.txt", "utf8").trim().split("\n"));
