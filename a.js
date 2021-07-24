504ed22a06cb40922c6bde2cd8ee86c8e1caa63b
Ryuki Goto <rth510ryu@gmail.com>
2021-07-24 21:26:48 +0900

    Blood Pressure

--- a.js
+++ a.js
@@ -1,13 +1,10 @@
-//A - Counting
+//Blood Pressure
 "use strict";
 
 const main = (lines) => {
   const [a, b] = lines[0].split(" ").map(Number);
-  let cnt = 0;
-  for (let i = a; i <= b; i++) {
-    cnt++;
-  }
-  console.log(cnt);
+  const c =  (a - b) / 3 + b
+  console.log(c);
 };
 
 main(require("fs").readFileSync("a.txt", "utf8").trim().split("\n"));
