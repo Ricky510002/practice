5b54ada0fcb930be415d6455fce4177746868811
Ryuki Goto <rth510ryu@gmail.com>
2021-08-05 14:57:53 +0900

    C - Average Length

--- 1.js
+++ 1.js
@@ -1,19 +1,30 @@
-//B - Training Camp
+//C - Average Length
 "use strict";
 
 const main = (lines) => {
-  let n = Number(lines[0]);
-  let power = 1;
-  let a = Math.pow(10, 9) + 7;
-
-  for (let i = 1; i <= n; i++) {
-    power = (power * i) % a;
+  lines = lines.trim().split("\n");
+  let n = parseInt(lines[0], 10);
+  lines.shift();
+  let a = [];
+  for (let i = 0; i < lines.length; i++) {
+    let t = lines[i].split(" ").map((val) => parseInt(val, 10));
+    a.push(t);
   }
 
-  console.log(power);
+  let sum = 0;
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      sum += Math.sqrt((a[i][0] - a[j][0]) ** 2 + (a[i][1] - a[j][1]) ** 2);
+      count++;
+    }
+  }
+
+  console.log((sum * (n - 1)) / count);
 };
 
-main(require("fs").readFileSync("stdin.txt", "utf8").trim().split("\n"));
+// main(require("fs").readFileSync("stdin.txt", "utf8").trim().split("\n"));
+main(require("fs").readFileSync("stdin.txt", "utf8"));
 
 //提出するときはこっちのファイルから読み込み
 //main(require("fs").readFileSync("/dev/stdin", "utf8").trim().split("\n"));
